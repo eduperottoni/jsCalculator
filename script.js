319681be9249e33a7b2492b246be76cc447ef549
Eduardo Perottoni <edu.perottoni@gmail.com>
2021-03-27 02:36:33 -0300

    Alterando elemento de visualização do input

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const btnEqual = document.querySelector('#equal')
-const input = document.querySelector('#view-fscreen-input')
+const inputDiv = document.querySelector('#view-fscreen-input')
 const resDiv = document.querySelector('#view-rscreen')
 
 function printOnInput(value){
-    input.value += `${value}`  
+    inputDiv.innerHTML += `${value}`  
 }
 
 function calculate(){}
