571a80845902483f64ef3a43723f611aad62abf8
Eduardo Perottoni <edu.perottoni@gmail.com>
2021-03-27 00:05:35 -0300

    Finalizando funcionalidade de mostrar a expressão na tela de input

---
+++ script.js
@@ -0,0 +1,10 @@
+const btnEqual = document.querySelector('#equal')
+const input = document.querySelector('#view-fscreen-input')
+const resDiv = document.querySelector('#view-rscreen')
+
+function printOnInput(value){
+    input.value += `${value}`  
+}
+
+function calculate(){}
+
