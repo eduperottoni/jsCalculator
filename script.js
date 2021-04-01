072632c71018440d04d174b683fb82d3b7370150
Eduardo Perottoni <edu.perottoni@gmail.com>
2021-03-31 23:40:40 -0300

    Adicionando as funcionalidades de cálculo

--- script.js
+++ script.js
@@ -1,14 +1,54 @@
 const btnEqual = document.querySelector('#equal')
-const inputDiv = document.querySelector('#view-fscreen-hist')
-const resDiv = document.querySelector('#view-rscreen')
+const input = document.querySelector('#view-fscreen-hist')
+const resInput = document.querySelector('#view-rscreen')
 
 function printOnInput(value){
-    inputDiv.value += `${value}`
-    console.log(inputDiv.value)
+    input.value += `${value}`
+    console.log(resInput.value)
 }
+
+/*FUNCTION VALIDATION:
+    Se o valor do input for inválido, retorna undefined
+    Se o valor for válido, retorna o número sem casas decimais 
+*/
+function validation(){
+    var inputtxt = input.value
+    try{
+        var result = (eval(inputtxt.replace('x','*')))//Validação para a operação de multiplicação 
+    }catch(e){ 
+        if (e instanceof SyntaxError){
+            console.log('Tem erro aí')
+            result = undefined
+        }
+    }
+
+    if (result == undefined){
+        return undefined
+    }else if(result - Number.parseInt(result) == 0){
+        return result
+    }else{
+        return result.toFixed(8)
+    }
+    
+}
+
 
 function calculate(){
+    var result = validation()
+    console.log(result)
+    resInput.value = result
+    input.value = resInput.value
 }
 
+function c(){
+    if (resInput.value == ''){
+        input.value = ''
+    }else{
+        input.value = resInput.value
+    }
+}
 
-//FAZER 
+function ce(){
+    input.value = ''
+    resInput.value = ''
+}
